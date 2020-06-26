007a1dc50516ecc843336074440b421c0fb594a7
William Lawrence <will.lawrence.3rd@gmail.com>
2020-06-26 05:33:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,2 +1,7 @@
-describe "prime?" do
-  it 'returns true for prime numbers' do
+def prime?(number_to_test)
+  return false if number_to_test < 2
+  (2..Math.sqrt(number_to_test)).each do |num|
+    return false if number_to_test % num == 0
+  end
+  true
+end 
